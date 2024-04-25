1748095170b11e1ef633d56a56f575f5481e1c19
Kyrie Lin <kyrielin66@gmail.com>
2024-04-25 11:55:16 -0700

    Contains Duplicate

---
+++ ArraysAndHashing/217-Contains-Duplicate.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
+
+# Example 2:
+# Input: nums = [1,2,3,4]
+# Output: false
+
+# Example 3:
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+class Solution:
+    def containsDuplicate(self, nums) -> bool:
+        # Create a set to store the numbers that have been seen
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                # If the number is already in the set, return True
+                return True
+            # Otherwise, add the number to the set
+            seen.add(num)
+
+        # If the loop completes, return False
+        return False
