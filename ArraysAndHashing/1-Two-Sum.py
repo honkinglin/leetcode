2d1038101c39b70d9410649e071b874621e165cc
Kyrie Lin <kyrielin66@gmail.com>
2024-04-27 10:29:00 -0700

    Two Sum

---
+++ ArraysAndHashing/1-Two-Sum.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/two-sum/
+# Solution: https://www.youtube.com/watch?v=KLlXCFG5TnA
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+class Solution:
+    def twoSum(self, nums, target: int):
+        # use hashMap
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            # check if diff is in the hashMap
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            # if not, add the number to the hashMap
+            prevMap[n] = i
+        
+        return []
